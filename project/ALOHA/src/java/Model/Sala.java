2109d00a64c655084f62656c468c4783e692e445
Marcos de Alencar Carvalho <marcoscarvalho1997@gmail.com>
2018-05-25 10:53:38 -0300

    Trabalhando em cadastro de sala

--- project/ALOHA/src/java/Model/Sala.java
+++ project/ALOHA/src/java/Model/Sala.java
@@ -4,18 +4,22 @@ package Model;
 
 public class Sala {
 
-    private String codigo;
+    //Identificador para o banco
+    private int id;
+    //Código para o arquivo .dat
+    private String codigoModelo;
+    
     private int capacidade;
     private String nome;
     private String tipo;
     private String bloco;
 
-    public String getCodigo() {
-        return codigo;
+    public String getCodigoModelo() {
+        return codigoModelo;
     }
 
-    public void setCodigo(String codigo) {
-        this.codigo = codigo;
+    public void setCodigoModelo(String codigo) {
+        this.codigoModelo = codigo;
     }
 
     public int getCapacidade() {
@@ -50,6 +54,15 @@ public class Sala {
         this.bloco = Bloco;
     }
 
+    public int getId() {
+        return id;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+    
+
     public Sala(int capacidade, String nome, String tipo, String bloco) {
         this.capacidade = capacidade;
         this.nome = nome;
@@ -61,7 +74,12 @@ public class Sala {
     }
     
     public String gerarCodigo(){
-        return null;
+        String cod;
+        if(id<10) cod = this.nome.substring(0,2)+"000"+this.id;
+        else if(id<100) cod = this.nome.substring(0,2)+"00"+this.id;
+        else if(id<1000) cod = this.nome.substring(0,2)+"0"+this.id;
+        else cod = this.nome.substring(0,2)+this.id;
+        return cod;
     }
     
     
